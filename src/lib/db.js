4dc42ced5268418379e32240035d86e01ecbdead
edwin jude <edwin.judefrancis@gmail.com>
2025-02-13 14:28:27 +0530

    mongodb connected

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,5 +1,10 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-export const connectDB = async (){
-    
-}
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONOGODB_URL);
+    console.log(`Mongodb conneted ${conn.connection.host}`);
+  } catch (error) {
+    console.log("Mongodb connection error", error);
+  }
+};
