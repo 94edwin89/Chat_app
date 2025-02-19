545d7485d168ee951b97e3692118579319f29af7
edwin jude <edwin.judefrancis@gmail.com>
2025-02-19 23:12:33 +0530

    password hashed

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,27 @@
-export const signup = (req,res)=>{
-    const {email,password,fullName}=req.body
-try {
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+  const { email, password, fullName } = req.body;
+  try {
     // hased password
-} catch (error) {
-    
-}
+    if (password < 6) {
+      return res
+        .status(400)
+        .json({ message: "password must be atlest 6 characters" });
+    }
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "Email already exists" });
+
+    const salt = await bcrpt.genSalt(10);
+    const hashedPassword = await bcrpt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+  } catch (error) {}
 };
 
 export const login = (req, res) => res.send("login route");
