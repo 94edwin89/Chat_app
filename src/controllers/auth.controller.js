9fa5d13b96bdbc36287dd57f9d2a299e1332f2c1
edwin jude <edwin.judefrancis@gmail.com>
2025-03-07 21:31:38 +0530

    backend for upload photos

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloundinary from '../lib/cloudinary.js'
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -82,4 +83,17 @@ export const logout = (req, res) => {
     console.log("Error in login controller", error.message);
     res.status(500).json({ message: "internal server error" });
   }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile pic is required" });
+    }
+
+    await cloundinary
+  } catch (error) {}
 };
