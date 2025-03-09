04419ca28ea74708d9c65f0ed15c92bde3c0a4cf
edwin jude <edwin.judefrancis@gmail.com>
2025-03-09 22:22:10 +0530

    login& logout& update profile

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-import cloundinary from '../lib/cloudinary.js'
+import cloundinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -94,6 +94,27 @@ export const updateProfile = async (req, res) => {
       return res.status(400).json({ message: "Profile pic is required" });
     }
 
-    await cloundinary
-  } catch (error) {}
+    const uploadResponse = await cloundinary.uploader.upload(profilePic);
+    const updateUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        profilePic: uploadResponse.secure_url,
+      },
+      { new: true }
+    );
+
+    res.status(200).json(updateUser);
+  } catch (error) {
+    console.log("error in Profile:", error);
+    res.status(500).json({ message: "internal server error" });
+  }
+};
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("error in checkAuth controller", error.message);
+    res.status(500).json({ message: "internal server error" });
+  }
 };
