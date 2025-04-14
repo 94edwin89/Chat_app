870c892692ca88a298c9661f0b2b4eeba9356f71
edwin jude <edwin.judefrancis@gmail.com>
2025-04-14 21:21:07 +0530

    message router bug fixed

--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -3,6 +3,7 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import {
   getMessage,
   getUserForSidebar,
+  sendMessage,
 } from "../controllers/message.controller.js";
 
 const router = express.Router();
